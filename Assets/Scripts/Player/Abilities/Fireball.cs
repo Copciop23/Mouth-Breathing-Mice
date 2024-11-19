7eabc2b38589696b0157db01ac8f0a89a424a6ae
debiludebilas123 <30183161@nescol.ac.uk>
2024-11-19 18:58:36 +0000

    fireball test

--- Assets/Scripts/Player/Abilities/Fireball.cs
+++ Assets/Scripts/Player/Abilities/Fireball.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject fireballPrefab;
+    public Transform firePoint;
 
-    // Update is called once per frame
+    private bool isFacingRight = true;
+
     void Update()
     {
-        
+        // Check for direction change based on player input
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            isFacingRight = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            isFacingRight = true;
+        }
+
+        // Adjust fire point position based on direction
+        firePoint.localPosition = isFacingRight ? new Vector2(1, 0) : new Vector2(-1, 0);
+
+        // Shoot the fireball when 'F' is pressed
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ShootFireball();
+        }
+    }
+
+    void ShootFireball()
+    {
+        // Instantiate the fireball at the fire point position and rotation
+        GameObject fireball = Instantiate(fireballPrefab, firePoint.position, firePoint.rotation);
+
+        // Get the Rigidbody2D component of the fireball
+        Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            // Set the velocity of the fireball based on the facing direction
+            float direction = isFacingRight ? 1 : -1;
+            rb.linearVelocity = new Vector2(direction, 0) * Mathf.Abs(rb.linearVelocity.x);
+        }
     }
 }
