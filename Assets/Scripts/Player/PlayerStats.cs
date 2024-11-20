14a85b74ddc47a8650ae0b346d6c27023be6c912
Markas <markask20@gmail.com>
2024-11-20 17:14:57 +0000

    Player hp bar

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,36 +1,71 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+
 public class PlayerStats : MonoBehaviour
 {
     public int playerHealth;
-    public Slider slider;
+    [SerializeField] Slider slider;
+
+    private HashSet<Collider2D> processedColliders = new HashSet<Collider2D>();
+    private Vector3 respawnPosition; // Variable to store the respawn position
 
     void Start()
     {
         playerHealth = 100;
-        slider.value=100;
+        slider.value = playerHealth;
+
+        // Save the player's initial position as the respawn position
+        respawnPosition = transform.position;
     }
 
-     private void OnCollisionEnter2D (Collision2D collision) {
-        if (collision.gameObject.CompareTag("Spike")) {
-                doDamage(20);
-        }
-     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            Collider2D collider = collision.collider;
 
-     public void doDamage(int damage){
-        if (playerHealth <= 0){
+            if (!processedColliders.Contains(collider))
+            {
+                processedColliders.Add(collider);
+                doDamage(20);
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            Collider2D collider = collision.collider;
+
+            // Remove the collider from the processed set when the collision ends
+            processedColliders.Remove(collider);
+        }
+    }
+
+    public void doDamage(int damage)
+    {
+        if ((playerHealth-damage) <= 0)
+        {
             Die();
         }
-        if(playerHealth>0){
+        else if (playerHealth > 0)
+        {
             playerHealth -= damage;
+            slider.value = playerHealth;
         }
-        
     }
-    private void Die() {
+
+    private void Die()
+    {
         Debug.Log("Player died. :(");
-        playerHealth=100;
 
+        // Reset health and slider value
+        playerHealth = 100;
+        slider.value = playerHealth;
+
+        // Move player back to the respawn position
+        transform.position = respawnPosition;
     }
-
 }
-
